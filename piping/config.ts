08e571ee3c020cc3d21ac2e81011c9baf786aed1
oligami <s20032@tokyo.kosen-ac.jp>
2024-08-31 17:23:13 +0900

    save

--- piping/config.ts
+++ piping/config.ts
@@ -1,4 +1,4 @@
-// const PreUrl = "https://ppng.io/";
-const PreUrl = "http://153.121.71.248/piping-server/";
+const PreUrl = "https://ppng.io/";
+// const PreUrl = "http://153.121.71.248/piping-server/";
 
 export { PreUrl };
